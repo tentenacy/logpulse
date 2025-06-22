a1edb7bb05646d4c4ac41db6305547ee4e495fcc
tenacy <tentenacy@gmail.com>
2025-06-22 17:29:47 +0900

    프로젝트 생성

---
+++ src/main/java/com/tenacy/logpulse/LogpulseApplication.java
@@ -0,0 +1,13 @@
+package com.tenacy.logpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogpulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogpulseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tenacy/logpulse/LogpulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenacy.logpulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogpulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
