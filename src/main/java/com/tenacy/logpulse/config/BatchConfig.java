0c63030ff12b74f5feaaf147cc4d0747f68ef401
tenacy <tentenacy@gmail.com>
2025-06-24 16:15:09 +0900

    feat: 배치 기본 설정

---
+++ src/main/java/com/tenacy/logpulse/config/BatchConfig.java
@@ -0,0 +1,10 @@
+package com.tenacy.logpulse.config;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfig {
+    // 배치 잡 관련 설정들이 추가될 예정
+}
